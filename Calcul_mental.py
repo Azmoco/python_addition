3937e111e71f3e71ea6383bd751d08cb39b646c5
Naitroz <73950508+Naitroz@users.noreply.github.com>
2020-11-04 16:16:12 +0100

    Update Calcul_mental.py

--- Calcul_mental.py
+++ Calcul_mental.py
@@ -6,7 +6,7 @@ from random import randint
 def operation():
     """demande le type d'opérations"""
     type_operation = 0
-    while type_operation != "addition" and type_operation != "soustraction" and type_operation != "multiplication":
+    while type_operation not in ('addition', 'soustraction', 'multiplication'):
         type_operation = input("""quelle opération voulez-vous faire ?
 (tapez aide pour ouvrir une page d'aide)\n""")
         if type_operation == "aide":
@@ -15,7 +15,7 @@ Pour obtenir 10 additions, ecrivez "addition"\n
 Pour obtenir 10 soustraction, ecrivez "soustraction"\n
 Pour obtenir 10 multiplication, ecrivez "multiplication"\n
             """)
-        elif type_operation != "addition" and type_operation != "soustraction" and type_operation != "multiplication":
+        elif type_operation not in ('addition', 'soustraction', 'multiplication'):
             print("""Erreur_1: Votre réponse est invalide. il faut inscrire "addition" ,
 "soustraction" ou "multiplication".\n""")
     return type_operation
@@ -35,13 +35,13 @@ def lit_nombre_chiffre():
 def affiche_addition(valeur_max):
     """affichage de 10 additions"""
     reponses_juste = 0
-    for i in range(10):
+    for _ in range(10):
         valeur1 = randint(0, valeur_max)
         valeur2 = randint(0, valeur_max)
         calcul = valeur1 + valeur2
-        print(str(valeur1) + " + " + str(valeur2))
+        print("\n" + str(valeur1) + " + " + str(valeur2))
         reponse_utilisateur = input("Quel est le résultat de ce calcul ? ")
-        while reponse_utilisateur.isdigit() == False: #verification si reponse_utilisateur ne comporte que des chiffres
+        while reponse_utilisateur.isdigit() == False: #verif reponse_utilisateur soit des chiffres
             print("""Erreur_3: Votre réponse est invalide,
 merci d'écrire uniquement des chiffres ou nombres""")
             reponse_utilisateur = input("Quel est le résultat de ce calcul ? ")
@@ -53,7 +53,7 @@ merci d'écrire uniquement des chiffres ou nombres""")
 def affiche_soustraction(valeur_max):
     """affichage de 10 soustraction"""
     reponses_juste = 0
-    for i in range(10):
+    for _ in range(10):
         valeur1 = randint(0, valeur_max)
         valeur2 = randint(0, valeur_max)
         if valeur1 < valeur2:
@@ -75,11 +75,11 @@ merci d'écrire uniquement des chiffres ou nombres""")
 def affiche_multiplication(valeur_max):
     """affichage de 10 multiplication"""
     reponses_juste = 0
-    for i in range(10):
+    for _ in range(10):
         valeur1 = randint(0, valeur_max)
         valeur2 = randint(0, valeur_max)
         calcul = valeur1 * valeur2
-        print(str(valeur1) + " x " + str(valeur2))
+        print("\n" + str(valeur1) + " x " + str(valeur2))
         #utilisation de "x" pour que l'utilisateur comprenne * plus facilement
         reponse_utilisateur = input("Quel est le résultat de ce calcul ? ")
         while reponse_utilisateur.isdigit() == False:
@@ -94,7 +94,7 @@ merci d'écrire uniquement des chiffres ou nombres""")
 def relancer_programme():
     """sert à relancer le programme si l'utilisateur le souhaite"""
     demande_relance = input("Voulez-vous relancer le programme ?\n")
-    while demande_relance != "Oui" and demande_relance != "Non":
+    while demande_relance not in ('Oui', 'Non'):
         print("""Veuillez répondre "Oui" ou "Non".""")
         demande_relance = input("Voulez-vous relancer le programme ?\n")
     if demande_relance == "Oui":
@@ -117,9 +117,8 @@ def main():
         affiche_operation = affiche_multiplication(valeur_max)
 
     print(str(affiche_operation))
-    relance = relancer_programme()
+    relancer_programme()
     print("Au revoir, à bientôt.")
 
 main()
-#décomposer les affiche_addition/affiche_soustraction/affiche_multiplication
-#car ces fonctions doivent juste afficher
+#décomposer les affiche_... car ces fonctions doivent juste afficher
