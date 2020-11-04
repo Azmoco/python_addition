64d1bb4a4d54198de01a6c762c5a1bbf9f6e83d9
Azmoco <61981714+Azmoco@users.noreply.github.com>
2020-11-04 15:27:22 +0100

    patch d'erreurs pylint

--- projey.py
+++ projey.py
@@ -1,31 +1,45 @@
-from random import *
+from random import randint
 
 def operation():
     type_operation = 0
     while type_operation != "addition" and type_operation != "soustraction" and type_operation != "multiplication":
-        type_operation = input("""Bonjour,\nQuelle opération voulez-vous faire ? (tapez "aide" pour ouvrir une page d'aide)\n""")
+        type_operation = input("""quelle opération voulez-vous faire ?
+(tapez aide pour ouvrir une page d'aide)\n""")
         if type_operation == "aide":
-            print ("""
+            print("""
 Pour obtenir 10 additions, ecrivez "addition"\n
 Pour obtenir 10 soustraction, ecrivez "soustraction"\n
 Pour obtenir 10 multiplication, ecrivez "multiplication"\n
             """)
         elif type_operation != "addition" and type_operation != "soustraction" and type_operation != "multiplication":
-            print("""Votre réponse est invalide. il faut inscrire "addition" , "soustraction" ou "multiplication".\n""")
+            print("""Erreur_1: Votre réponse est invalide. il faut inscrire "addition" ,
+"soustraction" ou "multiplication".\n""")
     return type_operation
+
+
+def lit_nombre_chiffre():
+    """a"""
+    nombre_chiffre = input("Combien voulez-vous de chiffre dans vos opérations?\n")
+    valeur_max = 0
+    while nombre_chiffre == "0" or not nombre_chiffre.isnumeric():
+        print("Erreur_2: Veuillez entrer un nombre entier supérieur à 0")
+        nombre_chiffre = input("Combien voulez-vous de chiffre dans vos opérations?\n")
+    nombre_chiffre = int(nombre_chiffre)
+    return nombre_chiffre
 
 
 def affiche_addition(valeur_max):
     reponses_juste = 0
-    for i in range (10):
+    for i in range(10):
         valeur1 = randint(0, valeur_max)
         valeur2 = randint(0, valeur_max)
         calcul = valeur1 + valeur2
-        print (str(valeur1) + " + " + str(valeur2))
+        print(str(valeur1) + " + " + str(valeur2))
         reponse_utilisateur = input("Quel est le résultat de ce calcul ? ")
         while reponse_utilisateur.isdigit() == False: #verification si reponse_utilisateur ne comporte que des chiffres
-            print ("Votre réponse est invalide, merci d'écrire uniquement des chiffres ou nombres")
-            reponse_utilisateur = input("Quel est le résultat de ce calcul ? ")
+            print("""Erreur_3: Votre réponse est invalide,
+merci d'écrire uniquement des chiffres ou nombres""")
+            reponse_utilisateur = input("Quel est le résultat de ce calcul ?")
         if int(reponse_utilisateur) == int(calcul):
             reponses_juste += 1
     return reponses_juste
@@ -33,7 +47,7 @@ def affiche_addition(valeur_max):
 
 def affiche_soustraction(valeur_max):
     reponses_juste = 0
-    for i in range (10):
+    for i in range(10):
         valeur1 = randint(0, valeur_max)
         valeur2 = randint(0, valeur_max)
         if valeur1 < valeur2:
@@ -41,10 +55,12 @@ def affiche_soustraction(valeur_max):
             valeur1 = valeur2
             valeur2 = valeur_echange
         calcul = valeur1 - valeur2
-        print ("\n" + str(valeur1) + " - " + str(valeur2))
+        print("\n" + str(valeur1) + " - " + str(valeur2))
         reponse_utilisateur = input("Quel est le résultat de ce calcul ? ")
-        while reponse_utilisateur.isdigit() == False: #verification si reponse_utilisateur ne comporte que des chiffres
-            print ("Votre réponse est invalide, merci d'écrire uniquement des chiffres ou nombres")
+        while reponse_utilisateur.isdigit() == False:
+            #verification si reponse_utilisateur ne comporte que des chiffres
+            print("""Erreur_3: Votre réponse est invalide,
+merci d'écrire uniquement des chiffres ou nombres""")
             reponse_utilisateur = input("Quel est le résultat de ce calcul ? ")
         if int(reponse_utilisateur) == int(calcul):
             reponses_juste += 1
@@ -52,38 +68,22 @@ def affiche_soustraction(valeur_max):
 
 def affiche_multiplication(valeur_max):
     reponses_juste = 0
-    for i in range (10):
+    for i in range(10):
         valeur1 = randint(0, valeur_max)
         valeur2 = randint(0, valeur_max)
         calcul = valeur1 * valeur2
-        print (str(valeur1) + " x " + str(valeur2)) #utilisation de "x" pour que l'utilisateur comprenne * plus facilement
+        print(str(valeur1) + " x " + str(valeur2))
+        #utilisation de "x" pour que l'utilisateur comprenne * plus facilement
         reponse_utilisateur = input("Quel est le résultat de ce calcul ? ")
-        while reponse_utilisateur.isdigit() == False: #verification si reponse_utilisateur ne comporte que des chiffres
-            print ("Votre réponse est invalide, merci d'écrire uniquement des chiffres ou nombres")
+        while reponse_utilisateur.isdigit() == False:
+            #verification si reponse_utilisateur ne comporte que des chiffres
+            print("""Erreur_3: Votre réponse est invalide,
+merci d'écrire uniquement des chiffres ou nombres""")
             reponse_utilisateur = input("Quel est le résultat de ce calcul ? ")
         if int(reponse_utilisateur) == int(calcul):
             reponses_juste += 1
     return reponses_juste
 
-def lit_nombre_chiffre():
-    """a"""
-    nombre_chiffre = input("Combien voulez-vous de chiffre dans vos opérations?\n")
-    valeur_max = 0
-    while nombre_chiffre == "0" or not nombre_chiffre.isnumeric():
-        print("Veuillez entrer un nombre entier supérieur à 0")
-        nombre_chiffre = input("Combien voulez-vous de chiffre dans vos opérations?\n")
-    nombre_chiffre = int(nombre_chiffre)
-    return nombre_chiffre
-
-def relancer_programme():
-    demande_relance = input("Voulez-vous relancer le programme ?\n")
-    while demande_relance != "Oui" and demande_relance != "Non" :
-        print("""Veuillez répondre "Oui" ou "Non".""")
-        demande_relance = input("Voulez-vous relancer le programme ?\n")
-    if demande_relance == "Oui":
-        relance = main()
-    relance = None
-    return relance
 
 def main():
     type_operation = operation()
@@ -99,8 +99,6 @@ def main():
         affiche_operation = affiche_multiplication(valeur_max)
 
     print(str(affiche_operation))
-    relance = relancer_programme()
-    print("Au revoir, à bientôt.")
 
 main()
 #décomposer les affiche_addition/affiche_soustraction/affiche_multiplication car ces fonctions doivent juste afficher
